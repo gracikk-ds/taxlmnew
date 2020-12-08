205cdc5c906ce817fecc4acbd0100aa88f19afe1
gracikk-finance <58971870+gracikk-finance@users.noreply.github.com>
2020-12-08 14:43:14 +0300

    ass

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email = "grac20101@gmail.com",
     url = "https://github.com/gracikk-finance/taxlmnew",
     keywords = ["tax", "seldon", "lm"],
-    packages = ["taxlmnew",
-               "taxlmnew.lasthope"])
+    packages = ["taxlm",
+               "taxlm.lasthope"])
