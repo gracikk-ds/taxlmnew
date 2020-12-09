0eec7d6155e91b00b625ddb9818756b99b6693ef
gracikk-finance <58971870+gracikk-finance@users.noreply.github.com>
2020-12-09 15:47:11 +0300

    1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "taxlmnew",
-    version='1.0.0',
+    version='1.0.1',
     description = "LM for Tax forecasting",
     author = "Alex Gordeev",
     author_email = "grac20101@gmail.com",
