8f7cd7581530053f2350707c7c3209224219d794
gracikk-finance <58971870+gracikk-finance@users.noreply.github.com>
2020-12-09 16:20:16 +0300

    return

--- taxlm/lasthope/seldon_tax_class.py
+++ taxlm/lasthope/seldon_tax_class.py
@@ -296,11 +296,8 @@ class linear_model:
             y_test = self.data.loc[self.data.index >= self.data.index[-num_points], [self.target_var]]
             y_train = self.data.loc[self.data.index < self.data.index[-num_points], [self.target_var]]
             
-            import statsmodels
             x_train = self.data.loc[self.data.index < self.data.index[-num_points], exog_var_list]
-            x_train = statsmodels.tools.tools.add_constant(x_train, prepend=False, has_constant='skip')
             x_test = self.data.loc[self.data.index >= self.data.index[-num_points], exog_var_list]
-            x_test = statsmodels.tools.tools.add_constant(x_test, prepend=False, has_constant='skip')
 
             predictions_test = pd.DataFrame()
             predictions_train = pd.DataFrame()
